f6d02381c2b57fdebf7691ee4aa2a4d142ee270b
omarashrafelaraby <omarashrafelaraby@gmail.com>
2024-10-05 00:23:04 +0300

    commit2

--- src/main/java/org/example/AmazonAutomationTask.java
+++ src/main/java/org/example/AmazonAutomationTask.java
@@ -1,57 +1,64 @@
 package org.example;
+
 import org.jetbrains.annotations.NotNull;
 import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 import java.util.List;
 
 
 public class AmazonAutomationTask {
-    public static void main(String[] args) {
+    public static void main(String[] args){
         WebDriver driver = getWebDriver();
         driver.manage().window().maximize();
-        Actions actions = new Actions(driver);
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
 
-        WebElement changeLanguageButton = driver.findElement(By.xpath("//span[@class=\"icp-nav-link-inner\"]//span[@class=\"nav-line-2\"]"));
+  /*     WebElement changeLanguageButton = driver.findElement(By.xpath("//span[@class=\"icp-nav-link-inner\"]//span[@class=\"nav-line-2\"]"));
         changeLanguageButton.click();
 
         WebElement changeLanguageToEnglish = driver.findElement(By.xpath("//input[@value=\"en_AE\"]/following-sibling::i[@class=\"a-icon a-icon-radio\"]"));
         changeLanguageToEnglish.click();
 
         WebElement saveChangesButton = driver.findElement(By.xpath("//input[@class=\"a-button-input\"]"));
-        saveChangesButton.click();
+        saveChangesButton.click();*/
 
         WebElement loginButton = driver.findElement(By.xpath("//span[@id=\"nav-link-accountList-nav-line-1\"]"));
         loginButton.click();
 
         WebElement emailTextBox = driver.findElement(By.xpath("//input[@id=\"ap_email\"]"));
-        emailTextBox.sendKeys("omarashrafelaraby@gmail.com");
+        emailTextBox.sendKeys("01155502161");
 
         WebElement continueButton = driver.findElement(By.xpath("//input[@id=\"continue\"]"));
         continueButton.click();
 
 
-        WebElement passwordTextBox = driver.findElement(By.xpath("//input[@id=\"ap_password\"]"));
-        actions.sendKeys(Keys.ESCAPE).perform();
-        passwordTextBox.sendKeys("P@ssw0rd");
+        WebElement passwordTextBox = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//input[@id=\"ap_password\"]")));
+//        actions.sendKeys(Keys.ESCAPE).perform();
+        passwordTextBox.sendKeys("Semester-Re5tar");
 
         WebElement signInButton = driver.findElement(By.xpath("//input[@id=\"signInSubmit\"]"));
         signInButton.click();
 
+      /*  System.out.println("Please enter the OTP sent to your device and press Enter...");
+        new java.util.Scanner(System.in).nextLine(); */// Wait for user input
+
+
         WebElement allMenuButton = driver.findElement(By.id("nav-hamburger-menu"));
         allMenuButton.click();
 
-        WebElement videoGames = driver.findElement(By.linkText("Video Games"));
+        WebElement videoGames = wait.until(ExpectedConditions.elementToBeClickable(By.linkText("Video Games")));
         videoGames.click();
 
-        WebElement allVideoGames = driver.findElement(By.linkText("All Video Games"));
-        allVideoGames.click();
+//        WebElement allVideoGames = driver.findElement(By.linkText("All Video Games"));
+//        allVideoGames.click();
 
-        WebElement freeShippingCheckbox = driver.findElement(By.name("//a//div[@class=\"a-checkbox a-checkbox-fancy aok-float-left apb-browse-refinements-checkbox\"][1]"));
+        WebElement freeShippingCheckbox =wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//a//input[@id=\"apb-browse-refinements-checkbox_2\"]")));
         freeShippingCheckbox.click();
 
         WebElement newCheckBox = driver.findElement(By.xpath("//a[@aria-current=\"step\"]/following::span[text()='New']"));
@@ -65,20 +72,20 @@ public class AmazonAutomationTask {
 
 
         while (true) {
-            List<WebElement> products = driver.findElements(By.cssSelector(".s-result-item"));
-            for (WebElement product : products) {
-                String price = product.findElement(By.cssSelector(".a-price-whole")).getText();
-                if (15000 > Integer.parseInt(price.replace(",", ""))) {
-                    product.findElement(By.cssSelector(".a-button-input")).click();
+            List<WebElement> productsList = driver.findElements(By.cssSelector(".s-result-item"));
+            for (WebElement product : productsList) {
+                String priceTag = product.findElement(By.cssSelector(".a-price-whole")).getText();
+                if (15000 > Integer.parseInt(priceTag.replace(",", ""))) {
+                    WebElement priceFilter = product.findElement(By.cssSelector(".a-button-input"));
+                    priceFilter.click();
                 }
             }
-            if (driver.findElements(By.cssSelector(".a-last")).isEmpty()) {
-                driver.findElement(By.cssSelector(".a-last")).click();
-            } else {
+            if (driver.findElements(By.cssSelector(".s-pagination-item s-pagination-next")).isEmpty()) {
+                driver.findElement(By.cssSelector(".s-pagination-item s-pagination-next")).click();
+            }else{
                 break;
             }
         }
-
 
         WebElement cartButton = driver.findElement(By.id("nav-cart"));
         cartButton.click();
@@ -97,16 +104,21 @@ public class AmazonAutomationTask {
         String shippingFees = driver.findElement(By.id("sc-shipping-charge-buybox")).getText();
         System.out.println("Total Amount: " + totalAmount);
         System.out.println("Shipping Fees: " + shippingFees);
+        driver.quit();
     }
 
     private static @NotNull WebDriver getWebDriver() {
         ChromeOptions options = new ChromeOptions();
-        options.addArguments("--incognito");
+       // options.addArguments("--incognito");
         options.addArguments("disable-infobars");
         options.addArguments("disable-popup-blocking");
         options.addArguments("--password-manager-enabled=false");
         options.addArguments("--disable-blink-features=AutomationControlled");
         options.addArguments("--disable-notifications");
+        options.addArguments("--disable-save-password-bubble");
+        options.addArguments("--disable-application-cache");
+        options.addArguments("--disable-cache");
+
         WebDriver driver = new ChromeDriver(options);
         driver.get("https://www.amazon.eg/?language=en_AE");
         return driver;
